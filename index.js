ee2afe8c57e333783b254ade5b753f632ea645dd
richard.c.chen@gmail.com <richard.c.chen@gmail.com>
2018-06-23 19:52:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ function whisper(string)  {
   return string.toLowerCase()
 }
 
-function logShout(string)
-  console.log('HELLO!')
+function logShout(string) {
+  console.log(string.toUpperCase)
+  }
